dd09237a1e393ea3269016f25c8a62854f4711da
Dougal Graham <dougalg@gmail.com>
2023-01-16 12:28:14 +0700

    Day 1: Start setting up base HTML

--- a11y-day1/src/App.tsx
+++ a11y-day1/src/App.tsx
@@ -1,7 +1,41 @@
 import './App.css';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<header>
+				<h1>Minerva</h1>
+				<nav>
+					<ul>
+						<li>
+							<a href="/">Demos</a>
+						</li>
+						<li>
+							<a href="/">Features</a>
+						</li>
+						<li>
+							<a href="/">Posts</a>
+						</li>
+						<li>
+							<a href="/">Categories</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<section>
+					<h2 className="screenreader-only">Recent Posts</h2>
+				</section>
+				<section>
+					<h2>Editor&lsquo;s Choice</h2>
+				</section>
+				<section>
+					<h2>Explore Trending Topics</h2>
+				</section>
+			</main>
+			<footer>This is the footer</footer>
+		</>
+	);
 }
 
 export default App;
